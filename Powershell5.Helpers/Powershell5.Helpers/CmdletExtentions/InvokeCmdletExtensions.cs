6fc6a0766be645344d9b4ba09f14069ddb695437
Joshua Irwin <joshua@irwin.ws>
2019-03-24 08:15:25 -0700

    Adding the first few extension methods for using powershell C# cmdlets.

---
+++ Powershell5.Helpers/Powershell5.Helpers/CmdletExtentions/InvokeCmdletExtensions.cs
@@ -0,0 +1,59 @@
+﻿namespace Powershell5.Helpers.CmdletExtentions
+{
+    using System.Collections;
+    using System.Collections.ObjectModel;
+    using System.Management.Automation;
+
+    using Powershell5.Helpers.Models;
+
+    public static class InvokeCmdletExtensions
+    {
+        public static void InvokePsCmdlet(this Cmdlet _, string cmdletName, CmdletParameterSet parameters = null, IEnumerable pipelineInput = null)
+        {
+            using (var ps = PowerShell.Create(RunspaceMode.CurrentRunspace))
+            {
+                ps.AddCommand(cmdletName);
+
+                if (parameters != null)
+                {
+                    ps.AddParameters(parameters);
+                }
+
+                if (pipelineInput != null)
+                {
+                    ps.Invoke(pipelineInput);
+                }
+                else
+                {
+                    ps.Invoke();
+                }
+            }
+        }
+
+        public static Collection<T> InvokePsCommand<T>(
+            this Cmdlet _,
+            string cmdletName,
+            CmdletParameterSet parameters = null,
+            IEnumerable pipelineInput = null)
+        {
+            using (var ps = PowerShell.Create(RunspaceMode.CurrentRunspace))
+            {
+                ps.AddCommand(cmdletName);
+
+                if (parameters != null)
+                {
+                    ps.AddParameters(parameters);
+                }
+
+                if (pipelineInput != null)
+                {
+                    return ps.Invoke<T>(pipelineInput);
+                }
+                else
+                {
+                    return ps.Invoke<T>();
+                }
+            }
+        }
+    }
+}
