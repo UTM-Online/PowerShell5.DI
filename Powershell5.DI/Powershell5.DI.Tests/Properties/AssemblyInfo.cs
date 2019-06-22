5bf06f29bad27a0012800377e8a4c8e125028a61
Joshua Irwin <joshua@irwin.ws>
2019-06-22 13:38:19 -0700

    Adding a unit test and checking in fixes for bugs discovered through the unit test.

---
+++ Powershell5.DI/Powershell5.DI.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Powershell5.DI.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Powershell5.DI.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed13f1f6-050f-434b-8768-fab6101913bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Powershell5.DI/Powershell5.DI.Tests/TestModels/TestContainer.cs
@@ -0,0 +1,100 @@
+﻿namespace Powershell5.DI.Tests.TestModels
+{
+    using System;
+
+    using UTMO.Powershell5.DI.DI;
+
+    [PowerShellDiContainer]
+    public class TestContainer : IPowerShellDiContainer
+    {
+        public void Register<T>()
+            where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<T>(string name)
+            where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TInterface, TImplementation>()
+            where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<TInterface, TImplementation>(string name)
+            where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterFactory<TInterface, TImplementation>(Func<TInterface, TImplementation> factoryMethod)
+            where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type abstraction, Type implementation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type abstraction, Type implementation, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton<TInterface, TImplementation>()
+            where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton<TInterface, TImplementation>(Func<TInterface, TImplementation> factoryMethod)
+            where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton<TInterface, TImplementation>(string name)
+            where TImplementation : class, TInterface
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton(Type abstraction, Type implementation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterSingleton(Type abstraction, Type implementation, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type)
+        {
+            if (type == typeof(string))
+            {
+                return "DI Successful";
+            }
+            else
+            {
+                throw new Exception($"BAD TYPE REQUESTED. Type {typeof(Type).Name} is not a STRING.");
+            }
+        }
+
+        public object Resolve(Type type, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
