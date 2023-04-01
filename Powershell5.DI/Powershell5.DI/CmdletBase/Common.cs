885736c19f3cb10ee921bea7d1e07fe196976192
Josh Irwin <joshua@irwin.ws>
2023-04-01 12:49:26 -0700

    Adding error handling for properties that are missing required accessors

--- Powershell5.DI/Powershell5.DI/CmdletBase/Common.cs
+++ Powershell5.DI/Powershell5.DI/CmdletBase/Common.cs
@@ -1,35 +1,74 @@
 ﻿namespace UTMO.Powershell5.DI.CmdletBase
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Management.Automation;
-    using System.Reflection;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Management.Automation;
+  using System.Reflection;
+  using System.Text.RegularExpressions;
+  using UTMO.Powershell5.DI.DI;
+  using UTMO.Powershell5.DI.Exceptions;
 
-    using UTMO.Powershell5.DI.DI;
+  public static class Common
+  {
+    private static List<Exception> Exceptions;
 
-    public static class Common
+    public static void PerformFieldInjection<T>(this Cmdlet cmdlet) where T : Attribute, IShouldInject
     {
-        public static void PerformFieldInjection<T>(this Cmdlet cmdlet) where T : Attribute, IShouldInject
+      Exceptions = new List<Exception>();
+
+      var injectableProperties = cmdlet.GetType().GetProperties(BindingFlags.NonPublic | BindingFlags.Instance).Where(
+       p => p.CustomAttributes.Any(a => a.AttributeType == typeof(T))).ToList();
+
+      if (injectableProperties.Any())
+      {
+        foreach (var property in injectableProperties)
         {
-            List<PropertyInfo> injectableProperties = cmdlet.GetType().GetProperties(BindingFlags.NonPublic | BindingFlags.Instance).Where(
-                     p => p.CustomAttributes.Any(a => a.AttributeType == typeof(T))).ToList();
+          try
+          {
+            // ReSharper disable once SuggestVarOrType_BuiltInTypes
+            string resolverName = property.GetCustomAttribute<T>().Name;
+            var    targetType   = property.PropertyType;
 
-            if (injectableProperties.Any())
+            var instance = string.IsNullOrWhiteSpace(resolverName)
+                             ? InternalContainer.Container.Value.Resolve(targetType)
+                             : InternalContainer.Container.Value.Resolve(targetType, resolverName);
+
+            property.SetValue(cmdlet, instance);
+          }
+          catch (ArgumentException ae)
+          {
+            var matcher = new Regex("Property (?<Accessor>.*) method not found.", RegexOptions.Compiled);
+
+            if (matcher.IsMatch(ae.Message))
             {
-                foreach (var property in injectableProperties)
-                {
-                    // ReSharper disable once SuggestVarOrType_BuiltInTypes
-                    string resolverName = property.GetCustomAttribute<T>().Name;
-                    var targetType = property.PropertyType;
-
-                    var instance = string.IsNullOrWhiteSpace(resolverName)
-                                       ? InternalContainer.Container.Value.Resolve(targetType)
-                                       : InternalContainer.Container.Value.Resolve(targetType, resolverName);
-
-                    property.SetValue(cmdlet, instance);
-                }
+              var acc = matcher.Match(ae.Message);
+              var ex = new InjectablePropertyMissingSetterException(property.Name,
+                                                                    acc.Groups["Accessor"].Value);
+              Exceptions.Add(ex);
+              continue;
             }
+
+            Exceptions.Add(ae);
+          }
+          catch (Exception ex)
+          {
+            Exceptions.Add(ex);
+          }
         }
+
+        if (Exceptions.Any())
+        {
+          if (Exceptions.Count == 1)
+          {
+            throw Exceptions.First();
+          }
+          else
+          {
+            throw new AggregateException(Exceptions);
+          }
+        }
+      }
     }
+  }
 }
---
+++ Powershell5.DI/Powershell5.DI/Exceptions/InjectablePropertyMissingSetterException.cs
@@ -0,0 +1,18 @@
+﻿namespace UTMO.Powershell5.DI.Exceptions
+{
+  using System;
+  using System.Runtime.CompilerServices;
+
+  public class InjectablePropertyMissingSetterException : ApplicationException
+  {
+    public InjectablePropertyMissingSetterException(string propertyName, string missingAccessor) : base(message: $"The property \"{propertyName}\" is missing the following accessor \"{missingAccessor}\"")
+    {
+      this.propertyName = propertyName;
+      this.MissingAccessor = missingAccessor;
+    }
+
+    public string propertyName { get; }
+
+    public string MissingAccessor { get; }
+  }
+}
